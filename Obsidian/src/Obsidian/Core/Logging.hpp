d2d0f1f4a5ec08f617a108067fdff51e00995dd8
ggjorven <jorbenvanderwal@gmail.com>
2025-08-19 19:53:11 +0200

    Make assert settable by user.

--- Obsidian/src/Obsidian/Core/Logging.hpp
+++ Obsidian/src/Obsidian/Core/Logging.hpp
@@ -15,15 +15,17 @@ namespace Obsidian
         #define OB_LOG_ERROR(fmt, ...)       ::Nano::Log::PrintLvl<::Nano::Log::Level::Error>(fmt __VA_OPT__(,) __VA_ARGS__)
         #define OB_LOG_FATAL(fmt, ...)       ::Nano::Log::PrintLvl<::Nano::Log::Level::Fatal>(fmt __VA_OPT__(,) __VA_ARGS__)
 
-        #define OB_ASSERT(x, fmt, ...)          \
-                    do                          \
-                    {                           \
-                        if (!(x))               \
-                        {                       \
-                            OB_LOG_FATAL("Assertion failed: ({0}), {1}.", #x, ::Nano::Text::Format(fmt __VA_OPT__(,) __VA_ARGS__)); \
-                            NANO_DEBUG_BREAK(); \
-                        }                       \
-                    } while (false)
+        #if !defined(OB_ASSERT)
+            #define OB_ASSERT(x, fmt, ...)          \
+                        do                          \
+                        {                           \
+                            if (!(x))               \
+                            {                       \
+                                OB_LOG_FATAL("Assertion failed: ({0}), {1}.", #x, ::Nano::Text::Format(fmt __VA_OPT__(,) __VA_ARGS__)); \
+                                NANO_DEBUG_BREAK(); \
+                            }                       \
+                        } while (false)
+        #endif
 
         #define OB_VERIFY(x, fmt, ...)          \
                     do                          \
